b9d43432a0a8979d8a58d01091b83eef3046263e
SebasR-25 <seruprisatur2703@gmail.com>
2025-05-18 18:29:29 -0500

    Tercer Commit, Premium Room
    Se hace la clase PremiumRomm que hereda de VIPRoom, se acaban de crear los métodos con los que se realizará el promedio de las calificaciones para los 3 tipos de habitaciones.

--- src/co/edu/uptc/model/VIPRoom.java
+++ src/co/edu/uptc/model/VIPRoom.java
@@ -2,6 +2,7 @@ package co.edu.uptc.model;
 
 public class VIPRoom extends Room {
 
+    private double interiorRate;
     private String[] sentences = new String[] {
             "Del 1 al 5, ¿cómo calificarías la limpieza?",
             "Del 1 al 5, ¿cómo calificarías la limpieza?", "Del 1 al 5, ¿cómo calificarías la comodidad?",
@@ -12,77 +13,26 @@ public class VIPRoom extends Room {
             "Del 1 al 5, ¿qué tan conforme está con el interior de la habitación?"
     };
 
-    private double cleaningRate;
-    private double comfortRate;
-    private double locationRate;
-    private double customerServiceRate;
-    private double qualityRate;
-    private double servicesRate;
-    private double interiorRate;
 
     public VIPRoom(String roomName) {
         super(roomName);
+        interiorRate = 0;
     }
 
     @Override
     public void makeRating() {
-        double temp = cleaningRate + comfortRate + locationRate + customerServiceRate + qualityRate + servicesRate+ interiorRate;
-        setGeneralRate(temp/7);
-    }
-
-    public void cleaningRating(double newCleaningRate){
-        if (cleaningRate != 0) {
-            cleaningRate = (cleaningRate+newCleaningRate)/2;
-        }else{
-            cleaningRate = newCleaningRate;
+        double temp = (getCleaningRate() + getComfortRate() + getLocationRate() + getCustomerServiceRate() + getQualityRate() + getServicesRate() + getInteriorRate())/7;
+        if (getGeneralRate() != 0) {
+            setGeneralRate((temp+getGeneralRate())/2);
         }
-    }
-
-    public void comfortRating(double newComfortRate){
-        if (cleaningRate != 0) {
-            cleaningRate = (cleaningRate+newComfortRate)/2;
-        }else{
-            cleaningRate = newComfortRate;
-        }
-    }
-
-    public void locationRating(double newLocationRate){
-        if (cleaningRate != 0) {
-            cleaningRate = (cleaningRate+newLocationRate)/2;
-        }else{
-            cleaningRate = newLocationRate;
-        }
-    }
-
-    public void customerServiceRating(double newCustomerServiceRate){
-        if (cleaningRate != 0) {
-            cleaningRate = (cleaningRate+newCustomerServiceRate)/2;
-        }else{
-            cleaningRate = newCustomerServiceRate;
-        }
-    }
-
-    public void qualityRating(double newQualityRate){
-        if (cleaningRate != 0) {
-            cleaningRate = (cleaningRate+newQualityRate)/2;
-        }else{
-            cleaningRate = newQualityRate;
-        }
-    }
-
-    public void servicesRating(double newServiceRate){
-        if (cleaningRate != 0) {
-            cleaningRate = (cleaningRate+newServiceRate)/2;
-        }else{
-            cleaningRate = newServiceRate;
-        }
+        setGeneralRate(temp);
     }
 
     public void interiorRating(double newInteriorRate){
-        if (cleaningRate != 0) {
-            cleaningRate = (cleaningRate+newInteriorRate)/2;
+        if (interiorRate != 0) {
+            interiorRate = (interiorRate+newInteriorRate)/2;
         }else{
-            cleaningRate = newInteriorRate;
+            interiorRate = newInteriorRate;
         }
     }
 
@@ -100,54 +50,6 @@ public class VIPRoom extends Room {
 
     public void setInteriorRate(double interiorRate) {
         this.interiorRate = interiorRate;
-    }
-
-    public double getCleaningRate() {
-        return cleaningRate;
-    }
-
-    public void setCleaningRate(double cleaningRate) {
-        this.cleaningRate = cleaningRate;
-    }
-
-    public double getComfortRate() {
-        return comfortRate;
-    }
-
-    public void setComfortRate(double comfortRate) {
-        this.comfortRate = comfortRate;
-    }
-
-    public double getLocationRate() {
-        return locationRate;
-    }
-
-    public void setLocationRate(double locationRate) {
-        this.locationRate = locationRate;
-    }
-
-    public double getCustomerServiceRate() {
-        return customerServiceRate;
-    }
-
-    public void setCustomerServiceRate(double customerServiceRate) {
-        this.customerServiceRate = customerServiceRate;
-    }
-
-    public double getQualityRate() {
-        return qualityRate;
-    }
-
-    public void setQualityRate(double qualityRate) {
-        this.qualityRate = qualityRate;
-    }
-
-    public double getServicesRate() {
-        return servicesRate;
-    }
-
-    public void setServicesRate(double servicesRate) {
-        this.servicesRate = servicesRate;
     }
 
 }
